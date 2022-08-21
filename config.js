d1ac23cc309288416c54467ecabce87d58fdd518
Arrafi231 <91902042+Arrafi231@users.noreply.github.com>
2022-08-21 21:56:25 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,16 +2,16 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 // Other
-global.owner = ['6285815975368']
+global.owner = ['6285774269597']
 global.premium = ['6285815975368']
-global.ownername = 'Liya'
-global.botname = 'AliyaBot'
-global.packname = '© by AliyaBot'
-global.gc = 'https://bit.ly/3KMlmv2'
-global.linkyt = 'https://youtube.com/channel/UC9Si3U0o9dGS9MDfJR5iF6Q'
-global.linkgc = 'https://chat.whatsapp.com/DwP6uHYBWBc6TBSsNJrzwN'
+global.ownername = 'Rafii'
+global.botname = 'Rafii-Md'
+global.packname = '© by RafiiBot'
+global.gc = '-'
+global.linkyt = '-'
+global.linkgc = '-'
 global.limitawal = '100'
-global.author = '@alya.xzy'
+global.author = '@rafii.xzy'
 global.sessionName = 'polo'
 global.prefa = ['','!','.','🐦','🐤','🗿']
 global.sp = 'terserah'
